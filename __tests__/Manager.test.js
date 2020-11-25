15869705cb428ad952c3dea87e3e777f7c2f661c
Ann Markham <ann.markham@outlook.com>
2020-11-25 14:37:25 -0700

    create manager object and pass test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,8 @@
+const Manager = require('../lib/Manager');
+
+test("Test the Manager", () => {
+  manager = new Manager("Fred Flintstone", 424242, "fred@bedrock.com", 402);
+
+  expect(manager.getRole()).toBe("Manager")
+  expect(manager.getOfficeNum()).toEqual(expect.any(Number));
+});
