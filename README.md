a734536dd3402a1bf944bd47dbb6c658ea135d90
Ann Markham <ann.markham@outlook.com>
2020-11-29 18:50:07 -0700

    add update from outside of assets directory

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Team Profile Generator
+
+## Description 
+A command line application that creates a record of Employees. 
+Cards will be generated for each employee - based on information input by the user.  
+All employees cards will show the Employee Name qand Position in the title, the employee id, and the employee's email address.
+Additional information for each employee role/title includes:
+Manager - Office Number,
+Engineer - gitHub username,
+Student - School.
+
+## Languages, Libraries and API's:   HTML, JavaScript, Bootstrap, node.js, Jest, 
+
+## Installation
+Launch from command line using node index.js
+
+## Usage 
+
+Answer questions when prompted.  When complete, open index.html to view page.  
+
+Screenshot of page: 
+![Screenshot](assets/images/Team Profile Generator.png)
+
+
+## Tests 
+Tested using npm Jest. 
+
--- src/page-template.js
+++ src/page-template.js
@@ -6,10 +6,8 @@ const Employee = require('../lib/Employee.js')
 const generateCards = teamMembers => {
     const teamArr = teamMembers.map(({ name, id, email, role, other }) => {
 
-        if (role === "Engineer") {
-            let teamCardHtml =
-
-                `
+        let teamCardHtml =
+            `` `
         <div class="col-sm">         
             <div class="card bg-primary" style="width: 18rem; margin-top: 20px;">   
                <div class="card-body">
@@ -18,56 +16,34 @@ const generateCards = teamMembers => {
                     <ul class="list-group">
                         <li class="list-group-item">Id: ${id}</li>
                         <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>  
-                       <li class="list-group-item">gitHub: <a href="other:${other}">${other}</a></li>  
-                    </ul>
+                        `
+        if (role === "Engineer") {
+
+            `<li class="list-group-item">gitHub: <a href="other:${other}">${other}</a></li> 
+            </ul>
                 </div>
             </div>
-        </div>
-        `
-            return teamCardHtml;
+        </div>`
+
+
         }
         else if (role === "Manager") {
-            let teamCardHtml =
-                `
-      
-            <div class="col-sm">
-            <div class="card bg-primary" style="width: 18rem; margin-top: 20px;">
-               <div class="card-body">
-                  <h2 class ="card-title text-white">${name}</h2>
-                  <h5 class="card-title text-white";" >${role}</h5>
-                    <ul class="list-group">
-                        <li class="list-group-item">Id: ${id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>  
-                        <li class="list-group-item other">"Office Number: "${other}</li> 
-                    </ul>
+            `<li class="list-group-item other">Office Number: ${other}</li> 
+            </ul>
                 </div>
             </div>
-        </div>
-        `
-            return teamCardHtml;
+        </div>`
         }
         else if (role === "Intern") {
-
-            let teamCardHtml =
-
-                `
-        <div class="col-sm">         
-            <div class="card bg-primary" style="width: 18rem; margin-top: 20px;">   
-               <div class="card-body">
-                  <h2 class ="card-title text-white">${name}</h2>
-                  <h5 class="card-title text-white";" >${role}</h5>
-                    <ul class="list-group">
-                        <li class="list-group-item">Id: ${id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>  
-                        <li class="list-group-item other">"School: " ${other}</li> 
-                    </ul>
+            ` <li class="list-group-item other">School: ${other}</li>
+            </ul>
                 </div>
             </div>
-        </div>
-        `
-            return teamCardHtml;
+        </div>`
         }
+        ``
 
+        return teamCardHtml;
 
     })
 
